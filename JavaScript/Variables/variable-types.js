07bcda79d6767df2201205e0bd0ed861b04e67a8
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 12:31:55 +0300

    revised variable types and thier sizes in memory

---
+++ JavaScript/Variables/variable-types.js
@@ -0,0 +1,21 @@
+
+let myInteger = 12; //32 bit number
+let myFloat = 10.8;
+let myLong = 1343547485858756; //64  bit number ()
+let myDouble = 15665875745673.85 ; // 64 bit floating point number
+let myBoolean = true ; //returns true or false based on a specific condition
+let notANumber = NaN; // not a number 
+let notANumberExample = 0/0; //Not possible; the output is NaN= not a Number
+let notDefined; //Not defined i.e undefined . We didn't define it ot anything else. Output: undefined
+let myNull = null; // null
+
+
+console.log(myFloat);
+console.log(myInteger);
+console.log(myLong);
+console.log(myBoolean);
+console.log(myDouble);
+console.log(notANumber);
+console.log(notANumber);
+console.log(notDefined);
+console.log(myNull);
