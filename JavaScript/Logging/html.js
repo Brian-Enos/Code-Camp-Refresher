8b154241b13f1f881391c6755b19dc12bd7bdda4
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 08:31:39 +0300

    Logged html elements

---
+++ JavaScript/Logging/html.js
@@ -0,0 +1,12 @@
+
+
+/* 
+You can log any element which exists within the DOM(Document Object Model).
+This can include but not limited to: body, html, p, section etc of the html document
+See the examples below
+*/
+console.log(document.body);
+console.log(document.h1);
+console.log(document.p);
+
+//etc etc 
