26aa8b1c2468748e2d1c7b009f21888b6eb003c6
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 13:57:23 +0300

    explored more on built in number constants

---
+++ JavaScript/Arrays-And-Objects/Number-Constants/number-constants.js
@@ -0,0 +1,12 @@
+
+//The Number constructor has some inbuilt constants that can be useful
+
+
+Number.MAX_VALUE ; //// 1.7976931348623157e+308
+Number.MAX_SAFE_INTEGER; // 9007199254740991
+Number.MIN_VALUE; // 5e-324
+Number.MIN_SAFE_INTEGER; // -9007199254740991
+Number.EPSILON; // 0.0000000000000002220446049250313
+Number.POSITIVE_INFINITY; // Infinity
+Number.NEGATIVE_INFINITY; // -Infinity
+Number.NaN; // NaN
