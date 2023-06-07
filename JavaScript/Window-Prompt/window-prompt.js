ac35608d08a3d26d9ec6bfb2ceedab9fef213f6a
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 10:07:46 +0300

    Used window alert method to prompt the user for input

---
+++ JavaScript/Window-Prompt/window-prompt.js
@@ -0,0 +1,13 @@
+
+/* 
+This is used to get input from the user
+see the example below
+
+syntax: window.prompt(text, [default]);
+
+text: The text displayed in the prompt box.
+default: A default value for the input field (optional)
+
+*/
+
+window.prompt("How old are you?: ");
