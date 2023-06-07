be9564d59eb9d42d235129528beaa422ea66cefc
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 11:06:41 +0300

    provision for images stored on a server is now included with appropriate description

---
+++ JavaScript/Using-The-Dom-API/Graphical-Text/images.js
@@ -0,0 +1,9 @@
+
+//If you have an image already pn the server, you could proceed as follows
+
+
+let img = document.createElement("img");
+
+img.src = "provide your url here" ;
+
+document.body.appendChild(img);
