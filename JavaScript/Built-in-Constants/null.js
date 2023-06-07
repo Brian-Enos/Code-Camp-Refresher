bca87cf4a11b7d1c9edebc5d283c1871046284df
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 12:59:33 +0300

    explored the nullcharacter and determined its relationship to undefined

---
+++ JavaScript/Built-in-Constants/null.js
@@ -0,0 +1,11 @@
+//used for representimg the intentional absence of an object value and its primitive value
+
+//null is equal to undefined but not equal to it
+
+let result = null == undefined ; //the output is "true"
+console.log(result);
+
+let result2 = null === undefined ; //would return fales
+
+
+console.log(result2);
