4b9eb8f01ebe3bd08253065a80a340dc2dde691f
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 10:17:13 +0300

    Used window confirm to display  a dialog box to a user

---
+++ JavaScript/Window Confirm/window-confirm.js
@@ -0,0 +1,12 @@
+
+/* 
+This displays a modal dialog with an optional message and two buttons: Ok and Cancel 
+*/
+let message = "Do you want to delte this file now? " ;
+let result = window.confirm(message);
+
+/*
+meassge: This is the optional string to be displayed in the dialog box
+result: will be assigned the boolean value depending on what the user enters(true for ok and false for cancel)
+*/
+
