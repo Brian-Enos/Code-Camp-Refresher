6788bfb1fbb95b6b97b179a4bcebaa9d26aef510
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 08:10:25 +0300

    concatenated two variables

---
+++ JavaScript/Logging/variable.js
@@ -0,0 +1,6 @@
+
+
+let thisVariable = "first value" ;
+let thatVariable = "Second Value" ;
+
+console.log("The first variable is: " + thisVariable + " and the second variable is: " + thatVariable );
