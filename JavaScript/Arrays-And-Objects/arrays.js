d1d148de8aa6bcbcf0e835dfa7aab59d57bf39dd
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 12:45:07 +0300

    revised arrays and assignment of element to them

---
+++ JavaScript/Arrays-And-Objects/arrays.js
@@ -0,0 +1,22 @@
+
+
+/*
+An array is a set of variables
+*/
+
+let myArray = []; //declared and initialised an empty arry
+let carsInParkingLot = ["Toyota" , "Lexus" , "Ferrari"]; //declared and initialised an array with content
+
+let favouriteFruits = ["Apple" , "Mango", "Oranges"];
+let employees = ["Billy", "Bob" , "Joe"];
+let randomVariables = [23, true , "Brian", "Shopping"];
+
+myArray = ["zero", "one", "two"];
+
+
+let elementNumber = 1;
+window.alert(myArray[elementNumber]); // should output element at index 1 which is "one"
+
+console.log(myArray); // assigning values to myArray array
+
+
