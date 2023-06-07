716ae67c4de2c9ee7ee4301f616cacd4ee90eddc
Brian-Enos <brianenos9198@gmail.com>
2023-06-07 12:17:31 +0300

    Manipulated two variables

---
+++ JavaScript/Variables/manipulate-variables.js
@@ -0,0 +1,9 @@
+
+let fistNumber =  25 ;
+let secondNumber =  10;
+
+let result =  fistNumber + secondNumber;
+
+console.log(result);
+console.log("This is the first number: " + fistNumber + " " + "and this is the second number: " + secondNumber + "."  + "The result of the above operation is: " + result);
+
