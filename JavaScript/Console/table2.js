36c42bf1317b27f3683ef64b48f7e7b702ac685d
Brian-Enos <brianenos9198@gmail.com>
2023-06-08 08:55:18 +0300

    More practice on tabulating output of objects

---
+++ JavaScript/Console/table2.js
@@ -0,0 +1,27 @@
+var personArr = [
+  {
+   "personId": 123,
+   "name": "Brian",
+   "city": "Nairobi",
+   "phoneNo": "1234567890"
+  },
+  {
+   "personId": 124,
+   "name": "Amelia",
+   "city": "Sydney",
+   "phoneNo": "1234567890"
+  },
+  {
+   "personId": 125,
+   "name": "Emily",
+   "city": "Perth",
+   "phoneNo": "1234567890"
+  },
+  {
+   "personId": 126,
+   "name": "Abraham",
+   "city": "Perth",
+   "phoneNo": "1234567890"
+  }
+  ];
+  console.table(personArr, ['name', 'personId']);
